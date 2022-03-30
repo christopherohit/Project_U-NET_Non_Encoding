12fbcf562469cc74300fd9ce2aaa204658e44d0a
Christopherohit <xuanquyen02023002@gmail.com>
2022-03-30 15:20:52 +0700

    create data struct

--- src/generator.py
+++ src/generator.py
@@ -1,43 +1,49 @@
 import ImageDataGenerator
+from preprocess_data.Preprocess_Data import adjust_data
 
 
-def train_generator(batch_size, train_path, image_folder, mask_folder, aug_dict,
-                    image_color_mode = 'grayscale',
-                    mask_color_mode = 'grayscale',
-                    image_save_prefix = 'image',
-                    mask_save_prefix = 'mask',
-                    save_to_dir = None,
-                    target_size = (256,256),
-                    seed = 1):
-  image_datagen = ImageDataGenerator(**aug_dict)
-  mask_datagen = ImageDataGenerator(**aug_dict)
+class Load_train_test():
+    def train_generator(batch_size, train_path, image_folder, mask_folder, aug_dict,
+                            image_color_mode = 'grayscale',
+                            mask_color_mode = 'grayscale',
+                            image_save_prefix = 'image',
+                            mask_save_prefix = 'mask',
+                            save_to_dir = None,
+                            target_size = (256,256),
+                            seed = 1):
+        image_datagen = ImageDataGenerator(**aug_dict)
+        mask_datagen = ImageDataGenerator(**aug_dict)
 
-  image_generator = image_datagen.flow_from_directory(
-      train_path,
-      classes = [image_folder],
-      class_mode = None,
-      color_mode = image_color_mode,
-      target_size = target_size,
-      batch_size = batch_size,
-      save_to_dir = save_to_dir,
-      save_prefix = image_save_prefix,
-      seed = seed
-  )
+        image_generator = image_datagen.flow_from_directory(
+            train_path,
+            classes = [image_folder],
+            class_mode = None,
+            color_mode = image_color_mode,
+            target_size = target_size,
+            batch_size = batch_size,
+            save_to_dir = save_to_dir,
+            save_prefix = image_save_prefix,
+            seed = seed
+        )
 
-  mask_generator = mask_datagen.flow_from_directory(
-      train_path,
-      classes = [mask_folder],
-      class_mode = None,
-      color_mode = mask_color_mode,
-      target_size = target_size,
-      batch_size = batch_size,
-      save_to_dir = save_to_dir,
-      save_prefix = mask_save_prefix,
-      seed = 1
-  )
+        mask_generator = mask_datagen.flow_from_directory(
+            train_path,
+            classes = [mask_folder],
+            class_mode = None,
+            color_mode = mask_color_mode,
+            target_size = target_size,
+            batch_size = batch_size,
+            save_to_dir = save_to_dir,
+            save_prefix = mask_save_prefix,
+            seed = 1
+        )
 
-  train_gen = zip(image_generator, mask_generator)
+        train_gen = zip(image_generator, mask_generator)
 
-  for (img, mask) in train_gen:
-    img, mask = adjust_data(img, mask)
-    yield (img, mask)
+        for (img, mask) in train_gen:
+            img, mask = adjust_data(img, mask)
+            yield (img, mask)
+            
+        def test_generator(test_files, target_size = (256, 256)):
+            for test_file in test_files:
+                yield test_load_image(test_file, target_size)
--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -1,6 +1,27 @@
+from logging import NullHandler
+import os
+from tkinter import filedialog
+from tkinter import *
+
+
 
 
 class Preprocess_Data():
+    root = Tk()
+    root.withdraw()
+    folder_selected = filedialog.askdirectory()
+    def create_dir(folder_selected):
+        
+        try:
+            isExist = os.path.exists(folder_selected)
+            if not isExist:
+                os.makedirs(folder_selected)
+                print("The new directory is created!")
+            else:
+                print("It's Exist")
+        except(Exception):
+            return 'Error'
+            
     def adjust_data(img, mask):
         img = img / 255
         mask = mask / 255
@@ -8,3 +29,27 @@ class Preprocess_Data():
         mask[mask <= 0.5] = 0
 
         return (img, mask)
+    
+    def Join_path(folder_selected):
+        INPUT_DIR = os.path.join('/content/gdrive/MyDrive/Dataset/CT Lung','src')
+        IMAGE_DIR = os.path.join('/content/gdrive/MyDrive/Dataset','CT Lung')
+        SEGMENTATION_DIR = os.path.join(INPUT_DIR, 'segmentation')
+        SEGMENTATION_TEST_DIR = os.path.join(SEGMENTATION_DIR, 'test')
+        SEGMENTATION_TRAIN_DIR = os.path.join(SEGMENTATION_DIR, 'train')
+        SEGMENTATION_AUG_DIR = os.path.join(SEGMENTATION_TRAIN_DIR, 'augmentation')
+        SEGMENTATION_IMAGE_DIR = os.path.join(SEGMENTATION_TRAIN_DIR, 'image')
+        SEGMENTATION_MASK_DIR = os.path.join(SEGMENTATION_TRAIN_DIR, 'mask')
+        SEGMENTATION_DILATE_DIR = os.path.join(SEGMENTATION_TRAIN_DIR, 'dilate')
+
+        SHENZEN_TRAIN_DIR = os.path.join(IMAGE_DIR, 'ChinaSet_AllFiles')
+
+
+        SHENZHEN_IMAGE_DIR = os.path.join(SHENZEN_TRAIN_DIR, "CXR_png")
+        SHENZHEN_MASK_DIR = os.path.join(IMAGE_DIR,"mask", "mask")
+
+        MONTGOMERY_TRAIN_DIR = os.path.join(IMAGE_DIR, "Montgomery", "MontgomerySet")
+        MONTGOMERY_IMAGE_DIR = os.path.join(MONTGOMERY_TRAIN_DIR, "CXR_png")
+        MONTGOMERY_LEFT_MASK_DIR = os.path.join(MONTGOMERY_TRAIN_DIR, "ManualMask", "leftMask")
+        MONTGOMERY_RIGHT_MASK_DIR = os.path.join(MONTGOMERY_TRAIN_DIR,"ManualMask", "rightMask")
+
+        DILATE_KERNEL = np.ones((15, 15), np.uint8)
